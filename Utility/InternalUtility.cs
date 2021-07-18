0b97bc2d041dec91f89534cb69152a959338303e
Vheos <Vheos777@gmail.com>
2021-07-18 03:49:23 +0200

    minimized and made internal

---
+++ Utility/InternalUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+
+namespace Vheos.Tools.ModdingCore
+{
+    static internal class InternalUtility
+    {
+        static public string SplitCamelCase(this string t)
+        => Regex.Replace(t, "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))", "$1 ");
+    }
+}
