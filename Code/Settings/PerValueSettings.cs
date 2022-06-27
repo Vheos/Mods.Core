4944946d36f31c3b73859a3b3c36c2f5d366e764
Vheos <Vheos777@gmail.com>
2022-06-27 05:42:38 +0200

    Added `PerValueSettings`

---
+++ Code/Settings/PerValueSettings.cs
@@ -0,0 +1,37 @@
+﻿namespace Vheos.Mods.Core;
+
+public abstract class PerValueSettings<TMod, TValue> where TMod : AMod
+{
+    // Settings
+    public ModSetting<bool> Header;
+    public PerValueSettings(TMod mod, TValue value, bool isToggle = false)
+    {
+        _mod = mod;
+        _value = value;
+        _isToggle = isToggle;
+
+        if (_isToggle)
+            Header = _mod.CreateSetting(Prefix + nameof(Header), false);
+    }
+    public virtual void Format()
+    {
+        if (_isToggle)
+            Header.Format(_value.ToString());
+        else
+            Header = _mod.CreateHeader(_value.ToString());        
+    }
+    public string Description
+    {
+        get => Header.Description;
+        set => Header.Description = value;
+    }
+    public ModSetting<T> CreateSetting<T>(string name, T defaultValue = default, AcceptableValueBase acceptableValues = null)
+    => _mod.CreateSetting(Prefix + name, defaultValue, acceptableValues);
+
+    // Privates
+    protected TMod _mod;
+    protected TValue _value;
+    protected bool _isToggle;
+    protected string Prefix
+    => $"{GetType().Name}_{_value}_";
+}
